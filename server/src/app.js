7c2fd9060de6b074132b0943b44f3324c254d0af
Dauren <d.esmuhanov3@gmail.com>
2023-07-25 23:58:05 +0600

    completed nasa project

--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
+const launchesRouter = require("./routes/launches/launches.router");
 
 // Middlewares
 app.use(
@@ -19,8 +20,9 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 
 // Routers
 app.use("/planets", planetsRouter);
-app.get("/", (req, res) => {
-  res.send(path.join(__dirname, "..", "public", "index.html"));
+app.use("/launches", launchesRouter);
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
 module.exports = app;
