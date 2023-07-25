aab7c22cecc8395d8de4b148d6aa977f9e01f41d
Dauren <d.esmuhanov3@gmail.com>
2023-07-25 12:10:32 +0600

    loading data on startup

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
 // Middlewares
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 
 // Routers
--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,13 +1,7 @@
-const planets = require("../../models/planets.model");
+const { planets } = require("../../models/planets.model");
 
 function getAllPlanets(req, res) {
-  return res.status(200).json({
-    status: "success",
-    results: planets.length,
-    data: {
-      planets,
-    },
-  });
+  return res.status(200).json(planets);
 }
 
 module.exports = {
