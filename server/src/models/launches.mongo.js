91c80d47354312dae8261b6be75dfda228cb0e88
Dauren <d.esmuhanov3@gmail.com>
2023-07-27 12:15:15 +0600

    MongoDB connection established

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const launchesSchema = mongoose.Schema({
+  flightNumber: {
+    type: Number,
+    required: true,
+  },
+  launchDate: {
+    type: Date,
+    required: true,
+  },
+  mission: {
+    type: String,
+    required: true,
+  },
+  rocket: {
+    type: String,
+    required: true,
+  },
+  target: {
+    type: mongoose.ObjectId,
+    required: true,
+  },
+  customers: [String],
+  upcoming: {
+    type: Boolean,
+    default: true,
+  },
+  success: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+// Connects launchesSchema with the "launches" collection
+module.exports = mongoose.model("Launch", launchesSchema);
--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,30 @@
 const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
+const mongoose = require("mongoose");
+
+const MONGO_URL =
+  "mongodb+srv://nasa-api:7diyhapSodUfJJ5J@cluster0.fd5uwul.mongodb.net/nasa?retryWrites=true&w=majority";
 
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", err => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}...`);
